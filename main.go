cf5865ff629762cefdbda10993b4a006e053d199
Luiz de Milon <kori@openmailbox.org>
2015-09-21 20:43:07 -0400

    Reorganize code so it's easier to understand, remove currently unused options, remove counters.

--- main.go
+++ main.go
@@ -5,30 +5,20 @@ import (
 	"fmt"
 	"sync"
 
-	"github.com/kori/gone/s/teknik"
-)
-
-var (
-	www  = flag.String("w", "", "Website to upload to.")
-	ctrs = flag.Bool("c", true, "Whether to display counters.")
+	"github.com/kori/gone/svc/teknik"
 )
 
 func main() {
 	flag.Parse()
 	var wg sync.WaitGroup
-	c := 1 // current file counter
 
 	// t = total files, p = path to file
 	for t, p := range flag.Args() {
 		wg.Add(1)
 		go func(file string) {
 			defer wg.Done()
-			f := teknik.Upload(p)
-			if *ctrs {
-				fmt.Print(c, "/", t+1, ": ")
-			}
-			fmt.Println(f)
-			c++
+			url := teknik.Upload(p)
+			fmt.Println(url)
 		}(p)
 	}
 	wg.Wait()
