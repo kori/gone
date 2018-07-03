3c2ba58f366c11be11100ef8dd92bb0d791b430d
Luiz de Milon <luiz.de.milon@gmail.com>
2018-07-03 02:12:34 -0400

    split teknik into a different package

--- main.go
+++ main.go
@@ -1,71 +1,14 @@
 package main
 
 import (
-	"bytes"
-	"crypto/tls"
-	"encoding/json"
 	"flag"
 	"fmt"
-	"io"
 	"log"
-	"mime/multipart"
-	"net/http"
 	"os"
 	"sync"
+
+	"github.com/kori/gone/services/teknik"
 )
-
-func Upload(url string, filepath string) (string, error) {
-	// Read file to be uploaded.
-	var b bytes.Buffer
-	w := multipart.NewWriter(&b)
-	f, err := os.Open(filepath)
-	if err != nil {
-		return "", err
-	}
-
-	// Write to form
-	fileForm, err := w.CreateFormFile("file", filepath)
-	if err != nil {
-		return "", err
-	}
-	io.Copy(fileForm, f)
-	// Close file and writer.
-	f.Close()
-	w.Close()
-
-	req, err := http.NewRequest("POST", url, &b)
-	if err != nil {
-		return "", err
-	}
-
-	req.Header.Set("Content-Type", w.FormDataContentType())
-
-	// Prepare client and call API
-	client := &http.Client{
-		Transport: &http.Transport{
-			TLSClientConfig: &tls.Config{
-				MinVersion: tls.VersionTLS11,
-				MaxVersion: tls.VersionTLS11,
-			},
-		},
-	}
-	// Unmarshal and return response.
-	res, err := client.Do(req)
-	if err != nil {
-		return "", err
-	}
-
-	var r struct {
-		Result struct {
-			URL string
-		}
-	}
-
-	dec := json.NewDecoder(res.Body)
-	dec.Decode(&r)
-
-	return r.Result.URL, nil
-}
 
 func main() {
 	var wg sync.WaitGroup
@@ -73,35 +16,29 @@ func main() {
 	flag.Parse()
 	if len(flag.Args()) == 0 {
 		log.Fatalln("Usage: gone [files]")
-	} else {
-		// Upload each file.
-		for _, p := range flag.Args() {
-			wg.Add(1)
-			go func(file string) {
-				if exists(file) {
-					link, err := Upload("https://api.teknik.io/v1/Upload", file)
-					if err != nil {
-						log.Fatalln(err)
-					}
-					fmt.Println(file+":", link)
-
-				} else {
-					log.Fatal("file doesn't exist: ", file)
-				}
-				wg.Done()
-			}(p)
-		}
-		wg.Wait()
 	}
+
+	// Upload each file.
+	for _, p := range flag.Args() {
+		wg.Add(1)
+		go func(file string) {
+			if exists(file) {
+				link, err := teknik.Upload(file)
+				if err != nil {
+					fmt.Println(file+":", "upload failed:", err)
+				}
+				fmt.Println(file+":", link)
+			} else {
+				fmt.Println("file doesn't exist:", file)
+			}
+			wg.Done()
+		}(p)
+	}
+
+	wg.Wait()
 }
 
 func exists(path string) bool {
 	_, err := os.Stat(path)
-	if err == nil {
-		return true
-	}
-	if os.IsNotExist(err) {
-		return false
-	}
-	return false
+	return !os.IsNotExist(err)
 }
---
+++ services/teknik/teknik.go
@@ -0,0 +1,68 @@
+package teknik
+
+import (
+	"bytes"
+	"crypto/tls"
+	"encoding/json"
+	"io"
+	"mime/multipart"
+	"net/http"
+	"os"
+)
+
+func Upload(filepath string) (string, error) {
+	// Read file to be uploaded.
+	var b bytes.Buffer
+	w := multipart.NewWriter(&b)
+	f, err := os.Open(filepath)
+	if err != nil {
+		return "", err
+	}
+
+	// Write to form
+	fileForm, err := w.CreateFormFile("file", filepath)
+	if err != nil {
+		return "", err
+	}
+	if _, err := io.Copy(fileForm, f); err != nil {
+		return "", err
+	}
+	// Close file and writer.
+	f.Close()
+	w.Close()
+
+	req, err := http.NewRequest("POST", "https://api.teknik.io/v1/Upload", &b)
+	if err != nil {
+		return "", err
+	}
+
+	req.Header.Set("Content-Type", w.FormDataContentType())
+
+	// Prepare client and call API
+	client := &http.Client{
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{
+				MinVersion: tls.VersionTLS11,
+				MaxVersion: tls.VersionTLS11,
+			},
+		},
+	}
+	// Unmarshal and return response.
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", err
+	}
+
+	var r struct {
+		Result struct {
+			URL string
+		}
+	}
+
+	dec := json.NewDecoder(resp.Body)
+	dec.Decode(&r)
+
+	resp.Body.Close()
+
+	return r.Result.URL, nil
+}
